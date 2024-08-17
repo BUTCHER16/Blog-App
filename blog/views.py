d5b9c9580f4a916c6c490a275f5eada04ad34452
BUTCHER16 <ravidossalbert@gmail.com>
2024-08-17 15:12:08 +0530

    Added CRUD in the Blog App

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
@@ -8,6 +8,16 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', content)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = '-date_posted'
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 def about(request):
     return render(request,'blog/about.html', {'title':'About'})
--- sample.py
+++ sample.py
@@ -1,9 +1,11 @@
-class Solution:
-    def gcdOfStrings(self, str1, str2):
-        r = len(str2)
-        if str2 in str1:
-            return str1[r:]
-        return ""
-        
-s = Solution()
-print(s.gcdOfStrings(str1 = "LEET", str2 = "CODE"))
+def canPlaceFlowers(flowerbed, n) :
+    if n==0:
+         return True
+    for i in range(len(flowerbed)):
+        if flowerbed[i] == 0 and ( (i==0) or (flowerbed[i-1] == 0) ) and (i == len(flowerbed)-1) or (flowerbed[i+1] == 0):
+            flowerbed[i] = 1
+            n -= 1
+            if n == 0:
+                return True
+
+print(canPlaceFlowers(flowerbed = [1,0,0,0,1], n = 1))
